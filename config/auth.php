0d7cf3fc1d0fc8d8be53f37a3a9d5cc0791af632
JapSeyz <Jesper@oestertoft.dk>
2016-06-16 23:58:08 +0200

    Added Volunteer Auth

--- config/auth.php
+++ config/auth.php
@@ -40,7 +40,6 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-
         'api' => [
             'driver' => 'token',
             'provider' => 'users',
@@ -69,11 +68,6 @@ return [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
         ],
-
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
     ],
 
     /*
