70a096dc92c7fb96e51ac3cbba91a1287044d61e
monkeyWzr <monkeywzr@gmail.com>
2016-10-07 20:29:08 +0800

    small changes :D

--- resources/views/Shared/Layouts/Master.blade.php
+++ resources/views/Shared/Layouts/Master.blade.php
@@ -38,7 +38,7 @@
 
     <div class="navbar-header">
         <a class="navbar-brand" href="javascript:void(0);">
-            <img style="width: 150px;" class="logo" alt="Attendize" src="{{asset('assets/images/logo-light.png')}}"/>
+            <img style="width: 150px;" class="logo" alt="Attendize" src="{{asset('assets/images/logo.png')}}"/>
         </a>
     </div>
 
