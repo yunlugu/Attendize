87252086345190ffda6fa3f91e4d266213fea9f0
monkeyWzr <monkeywzr@gmail.com>
2016-10-11 02:46:07 +0800

    change navbar links

--- resources/views/Public/Partials/Navbar.blade.php
+++ resources/views/Public/Partials/Navbar.blade.php
@@ -16,14 +16,14 @@
             <ul class="nav navbar-nav">
                 <li><a href="#">首页</a></li>
                 <li><a href="#">培训</a></li>
-                <li><a href="#">云麓园bbs</a></li>
-                <li><a href="#">Gitlab</a></li>
+                <li><a href="http://bbs.csu.edu.cn/">云麓园bbs</a></li>
+                <li><a href="http://git.yunlugu.org/">Gitlab</a></li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">加入我们</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">注册</a></li>
-                <li><a href="#">登录</a></li>
+                <li><a href="{{route('showSignup')}}">注册</a></li>
+                <li><a href="{{route('login')}}">登录</a></li>
             </ul>
 
         </div>
